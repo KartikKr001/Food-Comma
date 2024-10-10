3aaac0d27db12a7072de31112b5e90b63dbc1d25
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-10-10 21:07:52 +0530

    Added cloudinary config

--- src/config/ServerConfig.js
+++ src/config/ServerConfig.js
@@ -5,5 +5,8 @@ module.exports = {
     PORT : process.env.PORT,
     DB_URL : process.env.DB_URL,
     secret_key : process.env.JWT_SECRET,
-    JWT_expiry : process.env.JWT_expiry
+    JWT_expiry : process.env.JWT_expiry,
+    CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET : process.env.CLOUDINARY_API_SECRET
 }
